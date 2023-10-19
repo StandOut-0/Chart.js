21843efa2717061e1df58b509f3b13a8b68f787a
StandOut-0 <standout.first@gmail.com>
2023-10-19 14:16:10 +0900

    라인그래프 line : chart_line

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ Chart.defaults.borderColor = '#c1c1c1'; //그래프 수치선 색
 Chart.defaults.color = '#333333'; //텍스트 색
 
 // 막대그래프 bar
-const ctx = document.getElementById('chart_bar');
-new Chart(ctx, {
+const chart_bar = document.getElementById('chart_bar');
+new Chart(chart_bar, {
     type: 'bar',
     data: {
         labels: ['그래프1', '그래프2', '그래프3', '그래프4'],
@@ -18,13 +18,13 @@ new Chart(ctx, {
             borderWidth: 2,
 
             //background
-            backgroundColor: '#a59bf5'
+            backgroundColor: ['#a59bf0', '#c7c0f5','#998bf7', '#7b6fd5']
         }]
     },
     options: {
         //animation
         animation: true,
-
+        
         scales: { y: { beginAtZero: true } },
         //layout setting
         layout: {
@@ -34,8 +34,8 @@ new Chart(ctx, {
         plugins: {
             //legend font setting
             legend: { labels: { font: { 
-                family: "Arial", //Helvetica Neue, Helvetica, Arial, sans-serif
-                style: "italic", //normal, italic, oblique, initial, inherit
+                family: 'Arial', //Helvetica Neue, Helvetica, Arial, sans-serif
+                style: 'italic', //normal, italic, oblique, initial, inherit
                 size: 9
              } } }
         }
@@ -43,3 +43,15 @@ new Chart(ctx, {
 });
 
 
+// 라인그래프 line
+const chart_line = document.getElementById('chart_line');
+new Chart(chart_line, {
+    type: 'line',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7'],
+        datasets: [{
+            label: '라인그래프 line',
+            data: [10,3,30,23,10,5,50]
+        }]
+    }
+});
